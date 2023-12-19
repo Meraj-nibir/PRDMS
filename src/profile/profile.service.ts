0a512994674c8760b47b77648fde46493271e434
Meraj-UR-Rahman Nibir <75033388+Meraj-nibir@users.noreply.github.com>
2023-12-19 21:16:03 +0600

    Profile delete not implement

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,6 +1,6 @@
 // profile.service.ts
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Profile } from './entity/profile.entity';
@@ -22,7 +22,6 @@ export class ProfileService {
     const existingProfile = await this.profileRepository.findOne(userId);
 
     if (existingProfile) {
-
       await this.profileRepository.update(userId, { ...existingProfile, ...updatedProfile });
 
       const updated = await this.profileRepository.findOne(userId);
